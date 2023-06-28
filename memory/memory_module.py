c44a657d56578a638914e7541f14ec374d03a520
Nicolas Gatien <nicolas.gatien6@gmail.com>
2023-06-28 10:20:44 -0400

    📂 Moved Memory Module

--- memory/memory_module.py
+++ memory/memory_module.py
@@ -1,21 +1,23 @@
 import json
 import openai
 
-with open("config\\api_keys.json") as f:
-    keys = json.load(f)
-
-openai.api_key = keys["openai"]
-
-import numpy as np
+from datetime import datetime
 from sklearn.metrics.pairwise import cosine_similarity
 
-class TextEmbedding:
+class MemoryModule:
     def __init__(self, model_id="text-embedding-ada-002"):
         self.texts = []
         self.embeddings = []
         self.model_id = model_id
+        with open("config\\api_keys.json") as f:
+            self.keys = json.load(f)
 
-    def add_text(self, text):
+        openai.api_key = self.keys["openai"]
+
+    def add_memory(self, text):
+        now = datetime.now()
+        formatted_now = now.strftime("%d/%m/%Y %H:%M:%S")
+        text = formatted_now + " " + text
         # Get the embeddings from the OpenAI API
         response = openai.Embedding.create(
             input=[text],
@@ -26,7 +28,7 @@ class TextEmbedding:
         self.texts.append(text)
         self.embeddings.append(embedding)
 
-    def get_most_relevant_text(self, input_text):
+    def get_most_relevent_memories(self, input_text, top_n=3):
         # Get the embedding of the input text
         response = openai.Embedding.create(
             input=[input_text],
@@ -34,16 +36,8 @@ class TextEmbedding:
         )
         
         input_embedding = response['data'][0]['embedding']
-        similarities = cosine_similarity([input_embedding], self.embeddings)
-        most_relevant_index = np.argmax(similarities)
-        return self.texts[most_relevant_index]
+        similarities = cosine_similarity([input_embedding], self.embeddings)[0]
 
-# Create an instance of the class
-text_embedding = TextEmbedding(api_key="your-api-key-here")
-
-# Add texts
-text_embedding.add_text("The quick brown fox jumps over the lazy dog.")
-text_embedding.add_text("The five boxing wizards jump quickly.")
-
-# Get the most relevant text
-print(text_embedding.get_most_relevant_text("How quick is the fox?"))
+        # Get the indices of the top_n most similar texts
+        most_relevant_indices = similarities.argsort()[-top_n:][::-1]
+        return [self.texts[i] for i in most_relevant_indices]
